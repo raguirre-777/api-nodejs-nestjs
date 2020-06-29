bb5694246e03fa9fefa2f4693f16d2ce52a738db
Ricardo Aguirre <ric.aguirre@alumnos.duoc.cl>
2020-06-29 11:12:56 -0400

    prepariob to heroku

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -16,22 +16,18 @@ import { Roles } from '../role/decorators/role.decorator';
 import { RoleGuard } from '../role/guards/role.guard';
 import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 
-@ApiBearerAuth()
 @ApiTags('Mantenedor de Usuarios')
 @Controller('users')
 export class UserController {
   constructor(private readonly _userService: UserService) { }
 
   @Get(':id')
-  @Roles('PACIENTE', 'ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async getUser(@Param('id', ParseIntPipe) id: number): Promise<User> {
     const user = await this._userService.get(id);
     return user;
   }
 
-  @Roles('PACIENTE', 'ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
+
   @Get()
   async getUsers(): Promise<User[]> {
     const users = await this._userService.getAll();
@@ -39,31 +35,23 @@ export class UserController {
   }
 
   @Post()
-  @Roles('PACIENTE', 'ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async createUser(@Body() user: User): Promise<User> {
     const createdUser = await this._userService.create(user);
     return createdUser;
   }
 
   @Patch(':id')
-  @Roles('PACIENTE', 'ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async updateUser(@Param('id', ParseIntPipe) id: number, @Body() user: User) {
     const updatedUser = await this._userService.update(id, user);
     return true;
   }
 
   @Delete(':id')
-  @Roles('PACIENTE', 'ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async deleteUser(@Param('id', ParseIntPipe) id: number) {
     await this._userService.delete(id);
     return true;
   }
   @Post('setRole/:userId/:roleId')
-  @Roles('PACIENTE', 'ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async setRoleToUser(
     @Param('userId', ParseIntPipe) userId: number,
     @Param('roleId', ParseIntPipe) roleId: number,
