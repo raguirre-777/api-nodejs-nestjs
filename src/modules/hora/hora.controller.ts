51554d78f6089df7e8b153fe485ef5299bb10993
Ricardo Aguirre <ricardo.aguirre@egt.cl>
2020-07-15 02:34:12 -0400

    se quita auth

--- src/modules/hora/hora.controller.ts
+++ src/modules/hora/hora.controller.ts
@@ -20,60 +20,45 @@ import { RoleGuard } from '../role/guards/role.guard';
 
 @ApiTags('Solicitud de Hora')
 @Controller('hora')
-@ApiBearerAuth()
 export class HoraController {
   constructor(private readonly _horaService: HoraService) { }
 
   @Get(':id')
-  @Roles('PACIENTE', 'ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async getHora(@Param('id', ParseIntPipe) id: number): Promise<HoraDto> {
     const Hora = await this._horaService.get(id);
     return Hora;
   }
   @Get()
-  @Roles('PACIENTE', 'ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async getHoras(): Promise<HoraDto[]> {
     const horas = await this._horaService.getAll();
     return horas;
   }
 
   @Post('anular/:id')
-  @Roles('PACIENTE', 'ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async anularHora(@Param('id', ParseIntPipe) id: number) {
     await this._horaService.delete(id);
     return true;
   }
 
   @Post('solicitud/')
-  @Roles('PACIENTE', 'ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async pedirHora(@Body() hora: HoraDto): Promise<HoraDto> {
     const createdHora = await this._horaService.create(hora);
     return createdHora;
   }
 
   @Post()
-  @Roles('PACIENTE', 'ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async createHora(@Body() hora: HoraDto): Promise<HoraDto> {
     const createdHora = await this._horaService.create(hora);
     return createdHora;
   }
 
   @Patch(':id')
-  @Roles('PACIENTE', 'ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async updateHora(@Param('id', ParseIntPipe) id: number, @Body() hora: HoraDto) {
     const updatedHora = await this._horaService.update(id, hora);
     return true;
   }
 
   @Delete(':id')
-  @Roles('PACIENTE', 'ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async deleteHora(@Param('id', ParseIntPipe) id: number) {
     await this._horaService.delete(id);
     return true;
--- src/modules/orden-pedido/orden-pedido.controller.ts
+++ src/modules/orden-pedido/orden-pedido.controller.ts
@@ -20,68 +20,51 @@ import { RoleGuard } from '../role/guards/role.guard';
 
 @ApiTags('Solicitud Orden de Pedido')
 @Controller('orden_pedido')
-@ApiBearerAuth()
 export class OrdenPedidoController {
   constructor(private readonly _ordenPedidoService: OrdenPedidoService) { }
 
   @Get(':id')
-  @Roles('ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async getOrdenPedido(@Param('id', ParseIntPipe) id: number): Promise<OrdenPedidoDto> {
     const ordenPedido = await this._ordenPedidoService.get(id);
     return ordenPedido;
   }
   @Get()
-  @Roles('ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async getOrdenPedidos(): Promise<OrdenPedidoDto[]> {
     const ordenPedidos = await this._ordenPedidoService.getAll();
     return ordenPedidos;
   }
 
   @Post('recepcionar/:id')
-  @Roles('ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async recepcionarOrdenPedido(@Param('id', ParseIntPipe) id: number) {
     await this._ordenPedidoService.reception(id);
     return true;
   }
 
   @Post('anular/:id')
-  @Roles('ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async anularOrdenPedido(@Param('id', ParseIntPipe) id: number) {
     await this._ordenPedidoService.delete(id);
     return true;
   }
 
   @Post('solicitud/')
-  @Roles('ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async pedirOrdenPedido(@Body() ordenPedido: OrdenPedidoDto): Promise<OrdenPedidoDto> {
     const createdOrdenPedido = await this._ordenPedidoService.create(ordenPedido);
     return createdOrdenPedido;
   }
 
   @Post()
-  @Roles('ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async createOrdenPedido(@Body() ordenPedido: OrdenPedidoDto): Promise<OrdenPedidoDto> {
     const createdOrdenPedido = await this._ordenPedidoService.create(ordenPedido);
     return createdOrdenPedido;
   }
 
   @Patch(':id')
-  @Roles('ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async updateOrdenPedido(@Param('id', ParseIntPipe) id: number, @Body() ordenPedido: OrdenPedidoDto) {
     const updatedOrdenPedido = await this._ordenPedidoService.update(id, ordenPedido);
     return true;
   }
 
   @Delete(':id')
-  @Roles('ADMIN')
-  @UseGuards(AuthGuard(), RoleGuard)
   async deleteOrdenPedido(@Param('id', ParseIntPipe) id: number) {
     await this._ordenPedidoService.delete(id);
     return true;
